cc8b6f6842b96d6cf5d741d2e334e12c4d3d14b1
Gino Massei <ginomassei@Ginos-MacBook-Pro.local>
2020-08-19 18:30:08 -0300

    Created modules

---
+++ Participante.py
@@ -0,0 +1,5 @@
+class Participante():
+    def __init__(self, nombre, continente, ranking):
+        self.nombre = nombre
+        self.continente = continente
+        self.ranking = ranking
---
+++ style.py
@@ -0,0 +1,19 @@
+def print_red_text(string):
+    red_text = '\033[31;1m'
+    reset_text = '\033[m'
+
+    print(red_text + string + reset_text)
+
+
+def print_green_text(string):
+    green_text = '\033[32;1m'
+    reset_text = '\033[m'
+
+    print(green_text + string + reset_text)
+
+
+def print_blue_text(string):
+    blue_text = '\033[34;1m'
+    reset_text = '\033[m'
+
+    print(blue_text + string + reset_text)
