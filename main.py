70765a2404d05d8057408c847f658f7b9717bfac
ginomassei <ginomassei@icloud.com>
2020-08-29 10:45:47 -0300

    Now, the vector is generate outside the function.

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
 import resources as rs
 
 
-def option1():
-    rs.manual_load()
+def option1(participantes):
+    rs.manual_load(participantes)
 
 
-def option2():
-    rs.random_load()
+def option2(participantes):
+    rs.random_load(participantes)
 
 
 def main():
+    participantes = 16 * [None]  # Creación del vector contenedor de los participantes.
     option = 0
 
     while option != -1:
-        print('/nMenú de opciones:')
+        print('\nMenú de opciones:')
         print('1 - Cargar el vector de competidores de forma manual.')
         print('2 - Cargar el vector de competidores de forma automática.')
 
-        option = int(input('/nIngrese su opción: '))
+        option = int(input('\nIngrese su opción: '))
 
         if option == 1:
-            option1()
+            option1(participantes)
 
         if option == 2:
-            option2()
+            option2(participantes)
 
 
 if __name__ == "__main__":
--- resources.py
+++ resources.py
@@ -17,42 +17,33 @@ def to_string(participante):
     print(r)
 
 
-def random_load():
+def random_load(vec):
     # Lista con los 16 posibles nombres
     nombres = ['AED', 'MAD', 'ALG', 'SOR', 'ACO', 'AM1', 'ASI', 'AM2',
                'SSL', 'PPR', 'SOP', 'DSI', 'COM', 'GDA', 'SIM', 'ECO']
 
-    # Vector contenedor de participantes
-    v = 16 * [None]
-
     # Generación de un vector de participantes seleccionando los datos de forma aleatoria
-    for i in range(len(v)):
+    for i in range(len(vec)):
         nombre = random.choice(nombres)
         continente = random.randint(0, 4)
         ranking = random.randint(0, 100)
 
-        v[i] = Participante(nombre, continente, ranking)
+        vec[i] = Participante(nombre, continente, ranking)
 
         # Eliminación del nombre usado en la lista original
         nombres.remove(nombre)
-    return v
 
 
-def manual_load():
-    # Vector contenedor de participantes
-    v = 16 * [None]
-
+def manual_load(vec):
     # Generación de un vector de participantes cargando los datos manualmente
-    for i in range(len(v)):
+    for i in range(len(vec)):
 
         print('Competidor ' + str(i+1))
-
         nombre = input('Nombre del competidor: ')
         continente = verify_in_range(int(input('Continente(entre 0 y 4): ')), 0, 5)
         ranking = int(input('Ranking: '))
 
-        v[i] = Participante(nombre, continente, ranking)
-    return v
+        vec[i] = Participante(nombre, continente, ranking)
 
 
 def match_generation():
@@ -66,7 +57,6 @@ def match_simulation():
 def verify_in_range(num, n, x):
     while not(num in range(n, x)):
         print('Error. Debe ingresar un valor entre ' + str(n) + ' y ' + str(x - 1))
-
         num = int(input('Continente(entre 0 y 4): '))
 
     return num
